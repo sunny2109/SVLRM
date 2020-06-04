78057fa28b8e7648d9521e0cc69291e12fac5858
CS-SL <37045074+CS-SL@users.noreply.github.com>
2020-06-04 09:22:03 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,14 +3,14 @@ import argparse
 import time
 import math
 import numpy as np
-from data import DataGenerator
+from data import DataGenerator, TestDataGenerator
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from model import SVLRM as Net
 from loss import Loss 
-from utils import save_model, print_network
+from utils import save_model, print_network, calc_rmse_tensor, save_img
 from torchvision.utils import make_grid
 from torch.utils.tensorboard import SummaryWriter
 
@@ -35,12 +35,13 @@ if __name__ == '__main__':
     parser.add_argument('--n_gpus', type=int, default=1)
     parser.add_argument('--gpu_mode', type=bool, default=True)
     parser.add_argument('--snapshot_dir', type=str, default='./weights/')
-    parser.add_argument('--snapshot', type=int, default=5000)
+    parser.add_argument('--snapshot', type=int, default=100)
     parser.add_argument('--start_iter', type=int, default=1)
     parser.add_argument('--n_iters', type=int, default=20000000)
     parser.add_argument('--prinf_freq', type=int, default=10)
-    parser.add_argument('--test_data_dir', type=str, default='./dataset/test_depth_sr/')
-    parser.add_argument('--result_save_dir', type=str, default='./dataset/results/')
+    parser.add_argument('--val_freq', type=int, default=1000)
+    parser.add_argument('--val_data_dir', type=str, default='./dataset/val_data/')
+    parser.add_argument('--result_save_dir', type=str, default='./dataset/results/val_results/')
     parser.add_argument('--log_dir', type=str, default='./log/')
     parser.add_argument('--resume', type=bool, default=False)
     parser.add_argument('--resume_dir', type=str, default='./weights/X8/model_001.pth')
@@ -55,57 +56,22 @@ if __name__ == '__main__':
         os.makedirs(tblog_dir)
     writer = SummaryWriter(log_dir=tblog_dir, comment='batch_20_lr_1e-4_150w_epoch')
 
-    def train(training_data_loader, net, optim, criterion):
-        net.train()
-        curr_step = 0
-        while curr_step < opt.n_iters:
-            for _, batch in enumerate(training_data_loader, 1):
-                curr_step += 1
-                img_gt, img_input, img_guided = batch[0], batch[1], batch[2]
-
-                img_gt = img_gt.type(torch.FloatTensor).cuda()
-                img_input = img_input.type(torch.FloatTensor).cuda()
-                img_guided = img_guided.type(torch.FloatTensor).cuda()
-                
-                t0 = time.time()
-                img_pred, _, _ = net(img_input, img_guided)
-                loss = criterion(img_pred, img_gt) / opt.batch_size
-                t1 = time.time()
-                
-                optim.zero_grad()
-                loss.backward()
-                optim.step()
-
-                # learning rate is decayed with poly policy
-                lr_ = opt.lr * (1 - float(curr_step)/opt.n_iters)**2
-                for param_group in optim.param_groups:
-                    param_group['lr'] = lr_
-                
-                if curr_step % opt.prinf_freq == 0:
-                    print("===> Iters({}/{}): lr:{} || Loss: {:.4f} || Timer: {:.4f} sec.".format(curr_step, opt.n_iters, optim.param_groups[0]['lr'], loss.item(), (t1 - t0)))
-                    writer.add_scalar('Training_loss', loss.item(), curr_step)
-
-
-                if curr_step % opt.snapshot == 0:
-                    save_model(net, curr_step, opt.snapshot_dir, opt.upscaling_factor)
-
     # add code for datasets
     print("===> Loading datasets")
     data_set = DataGenerator(data_dir= opt.data_dir,
                 upscaling_factor = opt.upscaling_factor,
                 patch_size = opt.patch_size,
                 data_aug = opt.data_augment)
+    val_data_set = TestDataGenerator(data_dir= opt.val_data_dir, upscaling_factor = opt.upscaling_factor)
+
     training_data = DataLoader(dataset=data_set, batch_size=opt.batch_size, num_workers=opt.workers, shuffle=True, pin_memory=True)
+    val_data = DataLoader(dataset=val_data_set, batch_size=1, num_workers=opt.workers, shuffle=False)
     
     # instantiate network
     print("===> Building model")
     devices_ids = list(range(opt.n_gpus))
     net = Net()
 
-    # create losses
-    criterion_L1_cb = Loss(eps=1e-3)
-    criterion_L1_cb = criterion_L1_cb.cuda()
- 
     # if running on GPU and we want to use cuda move model there
     print("===> Setting GPU")
     cuda = opt.gpu_mode
@@ -113,8 +79,11 @@ if __name__ == '__main__':
         raise Exception("No GPU found, please run without --cuda")
     net = nn.DataParallel(net, device_ids=devices_ids)
     net = net.cuda()
-    
 
+    # create loss
+    criterion_L1_cb = Loss(eps=1e-3)
+    criterion_L1_cb = criterion_L1_cb.cuda()
+    
     print('---------- Networks architecture -------------')
     print_network(net)
     print('----------------------------------------------')
@@ -128,11 +97,67 @@ if __name__ == '__main__':
                 net.load_state_dict(torch.load(opt.resume_dir))
             print('Net work loaded from {}'.format(opt.resume_dir))
 
-    # create optimizers
+    # create optimizer
     print("===> Setting Optimizer")
     optim = torch.optim.Adam(net.parameters(), lr=opt.lr, betas=(0.9, 0.999), eps=1e-8, weight_decay=opt.weight_decay)
     
-print("===> Training")
-train(training_data, net, optim, criterion_L1_cb)
-writer.close()   
+    print("===> Training")
+    net.train()
+    curr_step = 0
+    while curr_step < opt.n_iters:
+        for _, batch in enumerate(training_data, 1):
+            curr_step += 1
+            img_gt, img_input, img_guided = batch[0], batch[1], batch[2]
+
+            img_gt = img_gt.type(torch.FloatTensor).cuda()
+            img_input = img_input.type(torch.FloatTensor).cuda()
+            img_guided = img_guided.type(torch.FloatTensor).cuda()
+            
+            t0 = time.time()
+            img_pred, _, _ = net(img_input, img_guided)
+            loss = criterion_L1_cb(img_pred, img_gt) / opt.batch_size
+            t1 = time.time()
+            
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            # learning rate is decayed with poly policy
+            lr_ = opt.lr * (1 - float(curr_step)/opt.n_iters)**2
+            for param_group in optim.param_groups:
+                param_group['lr'] = lr_
+            
+            if curr_step % opt.prinf_freq == 0:
+                print("===> Iters({}/{}): lr:{} || Loss: {:.4f} || Timer: {:.4f} sec.".format(curr_step, opt.n_iters, optim.param_groups[0]['lr'], loss.item(), (t1 - t0)))
+                writer.add_scalar('Training_loss', loss.item(), curr_step)
+
+
+            if curr_step % opt.val_freq == 0:
+                print('Evaluation....')
+                net.eval()
+                mean_rmse = 0
+                n_count, n_total = 1, len(val_data)
+                for batch in val_data:
+                    img_input, img_guided, img_gt = batch[0], batch[1], batch[2]
+                    img_input = img_input.type(torch.FloatTensor).cuda()
+                    img_guided = img_guided.type(torch.FloatTensor).cuda()
+                    img_gt = img_gt.type(torch.FloatTensor).cuda() 
+
+                    with torch.no_grad():
+                        results, _, _ = net(img_input, img_guided)
+
+                    rmse = calc_rmse_tensor(results, img_gt)
+                    mean_rmse += rmse
+
+                    image_name = val_data_set.imagefilenames[n_count-1]
+                    print("===> Processing: {}/{}".format(n_count, n_total))
+                    save_img(results.cpu(), image_name, opt.result_save_dir, opt.upscaling_factor)
+                    n_count += 1
+                mean_rmse /= len(val_data)
+                writer.add_scalar('Mean_RMSE', mean_rmse, curr_step)
+                print("Valid  iter [{}] || rmse: {}".format(curr_step, mean_rmse))
+                    
+            if curr_step % opt.snapshot == 0:
+                save_model(net, curr_step, opt.snapshot_dir, opt.upscaling_factor)
+    writer.close()   
         
